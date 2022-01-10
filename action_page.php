b57db7dbc5e99ca96e7dc79c3a87d060d7536d16
maffin1337 <maffin1337@mail.ru>
2022-01-10 09:58:43 +0300

    Changed action_page.php

--- action_page.php
+++ action_page.php
@@ -5,25 +5,47 @@ if(isset($_GET)){
        $email= $_GET["email"];
 }
 
+$pass_hash = password_hash($password, PASSWORD_DEFAULT);
+
 
 
 if (strpbrk($email, '@')) {
 	$html = htmlspecialchars($email);
 
-	echo "Регистрация проведена успешно";
+	echo "Регистрация проведена успешно\n";
 
 	$file = 'emails.txt';
 
 	$current = file_get_contents($file);
 
-	$current .= $email."   ".password_hash($password, PASSWORD_DEFAULT)."\n";
+	$current .= $email."   ".$pass_hash."\n";
 
 	$current .= "---\n";
 
 	file_put_contents ($file, $current);
 
-} else {
-	echo "E-mail введен неверно";
+	$link = mysqli_connect("localhost", "root", "", "user_information");
+
+	if (!$link) {
+		echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . 'ошибка: ' . mysqli_connect_error();
+	}
+
+	$sql_inf = "INSERT INTO `user` (`e-mail`, `password`) VALUES ('$email', '$password')";
+
+	if (mysqli_query($link, $sql_inf)){
+		echo "Записи успешно вставлены\n";
+	} else{
+		echo "ERROR: Не удалось выполнить $sql. " . mysqli_error($link);
+	}
+
+	$information = mysqli_query($link, 'SELECT `e-mail`, `password` FROM `user`');
+
+	while ($result = mysqli_fetch_array($information)){
+		echo "<br>E-mail: " . $result['e-mail'] . "Password: " . $result['password'] . "</br>";
+	}
+
+} else{
+	echo "Неправильно введен E-mail, повторите попытку.";
 }
 
 ?>
